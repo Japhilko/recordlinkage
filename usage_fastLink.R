16606c7abaccfe7c552cf6e03ce41b804ea3d479
Japhilko <JanPhilipp.kolb@gmail.com>
2020-12-21 10:25:33 +0100

    usage fastLink

---
+++ usage_fastLink.R
@@ -0,0 +1,13 @@
+# Jan-Philipp Kolb
+# Mon Dec 21 10:21:11 2020
+
+
+library(fastLink)
+
+
+
+# https://static.cambridge.org/content/id/urn:cambridge.org:id:article:S0003055418000783/resource/name/S0003055418000783sup001.pdf
+
+# https://imai.fas.harvard.edu/research/files/linkage.pdf
+
+# https://github.com/kosukeimai/fastLink
